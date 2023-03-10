c0e22fc85b8332f11ddfd2c5a8b8fccfa2be16f9
Chonkar-jay <120003007+Chonkar-jay@users.noreply.github.com>
2023-03-10 17:43:46 +0530

    GIT

---
+++ index.js
@@ -0,0 +1,13 @@
+popup()
+{
+    alert("I am an alert box!");
+}
+
+// function addMember() {
+//   var x = document.getElementsByClassName("infoForm");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
